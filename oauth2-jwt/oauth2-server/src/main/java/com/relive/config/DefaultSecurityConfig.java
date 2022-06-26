815f135301f45710cfa5e158de7b7109f9d09c30
ReLive27 <re_live27@163.com>
2022-06-26 17:26:57 +0800

    Delete JwtDecoder Bean

--- oauth2-jwt/oauth2-server/src/main/java/com/relive/config/DefaultSecurityConfig.java
+++ oauth2-jwt/oauth2-server/src/main/java/com/relive/config/DefaultSecurityConfig.java
@@ -1,13 +1,5 @@
 package com.relive.config;
 
-import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.JWSKeySelector;
-import com.nimbusds.jose.proc.JWSVerificationKeySelector;
-import com.nimbusds.jose.proc.SecurityContext;
-import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
-import com.nimbusds.jwt.proc.DefaultJWTProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,13 +9,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.HashSet;
-import java.util.Set;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -44,20 +31,6 @@ public class DefaultSecurityConfig {
                 )
                 .formLogin(withDefaults());
         return http.build();
-    }
-
-    @Bean
-    public JwtDecoder jwtDecoder(final JWKSource<SecurityContext> jwkSource) throws JOSEException {
-        Set<JWSAlgorithm> jwsAlgs = new HashSet();
-        jwsAlgs.addAll(JWSAlgorithm.Family.RSA);
-        jwsAlgs.addAll(JWSAlgorithm.Family.EC);
-        jwsAlgs.addAll(JWSAlgorithm.Family.HMAC_SHA);
-        ConfigurableJWTProcessor<SecurityContext> jwtProcessor = new DefaultJWTProcessor();
-        JWSKeySelector<SecurityContext> jwsKeySelector = new JWSVerificationKeySelector(jwsAlgs, jwkSource);
-        jwtProcessor.setJWSKeySelector(jwsKeySelector);
-        jwtProcessor.setJWTClaimsSetVerifier((claims, context) -> {
-        });
-        return new NimbusJwtDecoder(jwtProcessor);
     }
 
     @Bean
