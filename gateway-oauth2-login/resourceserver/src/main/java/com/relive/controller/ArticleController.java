c054633fa4ddbfd913f79ae2c5aa5781487e3e45
ReLive27 <re_live27@163.com>
2022-07-30 22:50:50 +0800

    Update `/resource/article`

--- gateway-oauth2-login/resourceserver/src/main/java/com/relive/controller/ArticleController.java
+++ gateway-oauth2-login/resourceserver/src/main/java/com/relive/controller/ArticleController.java
@@ -1,10 +1,13 @@
 package com.relive.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: ReLive
@@ -14,7 +17,10 @@ import java.util.List;
 public class ArticleController {
 
     @GetMapping("/resource/article")
-    public List<String> article() {
-        return Arrays.asList("article1", "article2", "article3");
+    public Map<String, Object> article(@AuthenticationPrincipal Jwt jwt) {
+        Map<String, Object> result = new HashMap<>(2);
+        result.put("principal", jwt.getClaims());
+        result.put("article", Arrays.asList("article1", "article2", "article3"));
+        return result;
     }
 }
