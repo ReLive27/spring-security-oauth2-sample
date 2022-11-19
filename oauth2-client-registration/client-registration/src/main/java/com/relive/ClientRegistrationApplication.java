3e83557cee837e84edf94469ccf6f43085149109
ReLive27 <re_live27@163.com>
2022-11-19 22:48:07 +0800

    Init project

---
+++ oauth2-client-registration/client-registration/src/main/java/com/relive/ClientRegistrationApplication.java
@@ -0,0 +1,16 @@
+package com.relive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ReLive
+ * @date: 2022/11/18 20:23
+ */
+@SpringBootApplication
+public class ClientRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientRegistrationApplication.class, args);
+    }
+}
