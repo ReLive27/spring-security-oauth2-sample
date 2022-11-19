0ac5dccf5649149c46701f976710c3345683aeec
ReLive27 <re_live27@163.com>
2022-11-19 22:54:57 +0800

    Init project

---
+++ oauth2-introspection-with-jwt/oauth2-server-introspction-with-jwt/src/main/java/com/relive/OAuth2ServerIntrospctionWithJwtApplication.java
@@ -0,0 +1,16 @@
+package com.relive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ReLive
+ * @date: 2022/11/18 21:08
+ */
+@SpringBootApplication
+public class OAuth2ServerIntrospctionWithJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2ServerIntrospctionWithJwtApplication.class, args);
+    }
+}
---
+++ oauth2-introspection-with-jwt/resourceserver-introspction-with-jwt/src/main/java/com/relive/ResourceServerApplication.java
@@ -0,0 +1,16 @@
+package com.relive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ReLive
+ * @date: 2022/11/16 21:02
+ */
+@SpringBootApplication
+public class ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerApplication.class, args);
+    }
+}
