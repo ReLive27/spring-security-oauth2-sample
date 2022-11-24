a3c8959e074a13927eeced16a11a97477d255a13
ReLive27 <re_live27@163.com>
2022-11-24 22:36:46 +0800

    Add the OpaqueTokenIntrospectorSupport attribute for the build OpaqueTokenIntrospector

--- oauth2-introspection-with-jwt/resourceserver-introspction-with-jwt/src/main/java/com/relive/authentication/IntrospectiveIssuerJwtAuthenticationManagerResolver.java
+++ oauth2-introspection-with-jwt/resourceserver-introspction-with-jwt/src/main/java/com/relive/authentication/IntrospectiveIssuerJwtAuthenticationManagerResolver.java
@@ -2,11 +2,13 @@ package com.relive.authentication;
 
 import com.relive.introspection.OAuth2Introspection;
 import com.relive.introspection.OAuth2IntrospectionService;
+import com.relive.introspection.OpaqueTokenIntrospectorSupport;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.log.LogMessage;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationManagerResolver;
 import org.springframework.security.oauth2.server.resource.authentication.OpaqueTokenAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import org.springframework.util.Assert;
 
 import java.util.Map;
@@ -23,9 +25,14 @@ public class IntrospectiveIssuerJwtAuthenticationManagerResolver implements Auth
 
     private final OAuth2IntrospectionService introspectionService;
 
-    public IntrospectiveIssuerJwtAuthenticationManagerResolver(OAuth2IntrospectionService introspectionService) {
+    private final OpaqueTokenIntrospectorSupport opaqueTokenIntrospectorSupport;
+
+    public IntrospectiveIssuerJwtAuthenticationManagerResolver(OAuth2IntrospectionService introspectionService,
+                                                               OpaqueTokenIntrospectorSupport opaqueTokenIntrospectorSupport) {
         Assert.notNull(introspectionService, "introspectionService can be not null");
+        Assert.notNull(opaqueTokenIntrospectorSupport, "opaqueTokenIntrospectorSupport can be not null");
         this.introspectionService = introspectionService;
+        this.opaqueTokenIntrospectorSupport = opaqueTokenIntrospectorSupport;
     }
 
     @Override
@@ -36,8 +43,8 @@ public class IntrospectiveIssuerJwtAuthenticationManagerResolver implements Auth
             AuthenticationManager authenticationManager = this.authenticationManagers.computeIfAbsent(issuer,
                     (k) -> {
                         log.debug("Constructing AuthenticationManager");
-                        //TODO 生成OpaqueTokenIntrospector
-                        return new OpaqueTokenAuthenticationProvider(null)::authenticate;
+                        OpaqueTokenIntrospector opaqueTokenIntrospector = this.opaqueTokenIntrospectorSupport.fromOAuth2Introspection(oAuth2Introspection);
+                        return new OpaqueTokenAuthenticationProvider(opaqueTokenIntrospector)::authenticate;
                     });
             log.debug(LogMessage.format("Resolved AuthenticationManager for issuer '%s'", issuer).toString());
             return authenticationManager;
