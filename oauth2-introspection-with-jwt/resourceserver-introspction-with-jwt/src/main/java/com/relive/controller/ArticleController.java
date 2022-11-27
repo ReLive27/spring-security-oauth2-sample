7861675c9914acd5c81714b09730b057b868c67d
ReLive27 <re_live27@163.com>
2022-11-27 22:12:17 +0800

    Adding a test endpoint

---
+++ oauth2-introspection-with-jwt/resourceserver-introspction-with-jwt/src/main/java/com/relive/controller/ArticleController.java
@@ -0,0 +1,27 @@
+package com.relive.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: ReLive
+ * @date: 2022/11/16 21:03
+ */
+@RestController
+public class ArticleController {
+
+    @GetMapping("/resource/article")
+    public Map<String, Object> foo(@AuthenticationPrincipal OAuth2AuthenticatedPrincipal principal) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("sub", principal.getAttribute("sub"));
+        result.put("articles", Arrays.asList("Effective Java", "Spring In Action"));
+
+        return result;
+    }
+}
