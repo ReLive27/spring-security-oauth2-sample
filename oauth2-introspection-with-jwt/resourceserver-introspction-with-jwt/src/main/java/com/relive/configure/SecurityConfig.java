3378f6a50a7f09768c60e6d0bbb9929180de8238
ReLive27 <re_live27@163.com>
2022-11-27 22:15:51 +0800

    Update SecurityConfig

---
+++ oauth2-introspection-with-jwt/resourceserver-introspction-with-jwt/src/main/java/com/relive/configure/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.relive.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author: ReLive
+ * @date: 2022/11/16 21:32
+ */
+@Configuration(proxyBeanMethods = false)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .mvcMatchers("/resource/article").hasAuthority("SCOPE_message.read")
+                        .anyRequest().authenticated()
+                )
+                .apply(new OAuth2IntrospectiveResourceServerAuthorizationConfigurer())
+                .opaqueTokenIntrospectorSupport();
+        return http.build();
+    }
+}
