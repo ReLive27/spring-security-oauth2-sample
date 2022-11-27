6220c2732ea4293842952f5683e8dbd87dab7fba
ReLive27 <re_live27@163.com>
2022-11-27 22:05:13 +0800

    Add OAuth2 client service

---
+++ oauth2-introspection-with-jwt/oauth2-client-introspection-with-jwt/src/main/java/com/relive/ClientApplication.java
@@ -0,0 +1,16 @@
+package com.relive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ReLive
+ * @date: 2022/11/27 20:04
+ */
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+}
---
+++ oauth2-introspection-with-jwt/oauth2-client-introspection-with-jwt/src/main/java/com/relive/controller/ClientTestController.java
@@ -0,0 +1,34 @@
+package com.relive.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
+
+/**
+ * @author: ReLive
+ * @date: 2022/11/27 20:07
+ */
+@RestController
+public class ClientTestController {
+
+    @Autowired
+    private WebClient webClient;
+
+    @GetMapping(value = "/client/test")
+    public Map<String, Object> getArticles(@RegisteredOAuth2AuthorizedClient("messaging-client-authorization-code") OAuth2AuthorizedClient authorizedClient) {
+        return this.webClient
+                .get()
+                .uri("http://127.0.0.1:8090/resource/article")
+                .attributes(oauth2AuthorizedClient(authorizedClient))
+                .retrieve()
+                .bodyToMono(Map.class)
+                .block();
+    }
+}
