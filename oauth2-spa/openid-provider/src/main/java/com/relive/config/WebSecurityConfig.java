e0b50415270674e7f8b3bd31216e8f8c567597b6
ReLive27 <re_live27@163.com>
2023-03-15 20:06:08 +0800

    Add OAuth2 resource service configuration

--- oauth2-spa/openid-provider/src/main/java/com/relive/config/WebSecurityConfig.java
+++ oauth2-spa/openid-provider/src/main/java/com/relive/config/WebSecurityConfig.java
@@ -3,18 +3,21 @@ package com.relive.config;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import com.relive.authentication.Http200LogoutSuccessHandler;
+import com.relive.authentication.Http401UnauthorizedEntryPoint;
 import com.relive.authentication.JwtAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationEntryPointFailureHandler;
 
 /**
  * @author: ReLive
@@ -33,20 +36,22 @@ public class WebSecurityConfig {
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests.anyRequest().authenticated()
                 )
-                .formLogin().successHandler(new JwtAuthenticationSuccessHandler(jwkSource))
+                .formLogin().successHandler(new JwtAuthenticationSuccessHandler(jwkSource)).failureHandler(new AuthenticationEntryPointFailureHandler(new Http401UnauthorizedEntryPoint()))
                 .and()
                 .logout().logoutSuccessHandler(new Http200LogoutSuccessHandler())
                 .and()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .csrf().disable();
+                .csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(new Http401UnauthorizedEntryPoint());
         return http.build();
     }
 
     @Bean
     UserDetailsService users() {
         UserDetails user = User.withUsername("admin")
-                .password("{noop}password")
+                .password("{noop}111111")
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user);
