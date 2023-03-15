f7c232146e718c25e36485ede99a0df643ee192d
ReLive27 <re_live27@163.com>
2023-03-15 20:06:52 +0800

    Add userInfo api

---
+++ oauth2-spa/openid-provider/src/main/java/com/relive/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.relive.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: ReLive
+ * @date: 2023/3/15 19:12
+ */
+@RestController
+public class UserController {
+
+    @GetMapping("/user/info")
+    public Map<String, Object> getUserInfo(@AuthenticationPrincipal Jwt jwt) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("code", HttpServletResponse.SC_OK);
+        result.put("data", Collections.singletonMap("name", jwt.getClaim("sub")));
+        return result;
+
+    }
+}
