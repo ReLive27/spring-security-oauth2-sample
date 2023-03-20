c33279a20fcaf9f39765273936d4636acbf4c75e
ReLive27 <re_live27@163.com>
2023-03-20 20:30:50 +0800

    Modify response data

--- oauth2-spa/relying-party/src/main/java/com/relive/controller/HomeController.java
+++ oauth2-spa/relying-party/src/main/java/com/relive/controller/HomeController.java
@@ -4,6 +4,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
+
 /**
  * @author: ReLive
  * @date: 2023/3/18 18:47
@@ -12,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/home")
-    public String home(Authentication authentication) {
-        return authentication.getName();
+    public Map<String, String> home(Authentication authentication) {
+        return Collections.singletonMap("name", authentication.getName());
     }
 }
