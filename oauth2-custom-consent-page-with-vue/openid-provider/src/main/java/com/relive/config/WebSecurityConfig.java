09334208ace1d32a76b049122518dcae7c8891ed
ReLive27 <re_live27@163.com>
2023-03-25 16:10:00 +0800

    Add Github OAuth2 client

--- oauth2-custom-consent-page-with-vue/openid-provider/src/main/java/com/relive/config/WebSecurityConfig.java
+++ oauth2-custom-consent-page-with-vue/openid-provider/src/main/java/com/relive/config/WebSecurityConfig.java
@@ -15,6 +15,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationEntryPointFailureHandler;
@@ -41,11 +46,32 @@ public class WebSecurityConfig {
                 .logout().logoutSuccessHandler(new Http200LogoutSuccessHandler())
                 .and()
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .oauth2Login(oauth2Login -> oauth2Login.successHandler(new JwtAuthenticationSuccessHandler(jwkSource)))
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(new Http401UnauthorizedEntryPoint());
         return http.build();
+    }
+
+    @Bean
+    ClientRegistrationRepository clientRegistrationRepository() {
+        //Please apply for the correct clientId and clientSecret on github
+        ClientRegistration clientRegistration = ClientRegistration.withRegistrationId("github")
+                .clientId("123456")
+                .clientSecret("123456")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("http://localhost:9528/oauth2/callback")
+                .scope(new String[]{"read:user"})
+                .authorizationUri("https://github.com/login/oauth/authorize")
+                .tokenUri("https://github.com/login/oauth/access_token")
+                .userInfoUri("https://api.github.com/user")
+                .userNameAttributeName("login")
+                .clientName("GitHub").build();
+
+
+        return new InMemoryClientRegistrationRepository(clientRegistration);
     }
 
     @Bean
